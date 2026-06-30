a4ed70c7d332d408d69971be5f94768509f48f81
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 17:25:04 +0000

    Jun 30, 2026, 10:55 PM

--- avi.java
+++ avi.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -57,7 +58,7 @@ public class Avi {
         //
         Solution s = new Solution();
         int[] arr = s.twoSum(new int[]{1,2,3,4,5},8);
-        System.out.println(arr);
+        System.out.println(Arrays.toString((arr)));
     }
 }
 
