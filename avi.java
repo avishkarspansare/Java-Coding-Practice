3d75f3bf7cadde44b4c1f6ce840421325d9b97e6
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 09:02:12 +0000

    Jun 30, 2026, 2:32 PM

--- avi.java
+++ avi.java
@@ -2,9 +2,8 @@
 public class Avi {
 
     public static void main(String[] args) {
-        String s = "Avishkar";
+        String s = "Ava";
         String rev = new StringBuilder(s).reverse().toString();
-        System.out.println(rev);
+        System.out.println(s.equals(rev) ? "Palindrome" : "Not Palindrome");
     }
 }
-
