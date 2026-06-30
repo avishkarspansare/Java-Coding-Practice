66f74012f3081a9e94b730aadc50f1b4ef6696a7
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 10:14:36 +0000

    Jun 30, 2026, 3:44 PM

--- avi.java
+++ avi.java
@@ -2,13 +2,6 @@
 public class Avi {
 
     public static void main(String[] args) {
-        int n = 1901;
-        int temp = n, rev = 0;
 
-        while (n != 0) {
-            rev = rev * 10 + n % 10;
-            n /= 10;
-        }
-        System.out.println((temp == rev) ? temp : rev);
     }
 }
