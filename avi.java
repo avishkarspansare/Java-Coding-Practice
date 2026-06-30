4b5d522c6950ed49e88ffaa50555ddef5ecb1f10
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 15:10:56 +0000

    Jun 30, 2026, 8:40 PM

--- avi.java
+++ avi.java
@@ -2,13 +2,16 @@
 public class Avi {
 
     public static void main(String[] args) {
-        int n = 1634;
-        int temp = n, sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum = sum + (digit * digit * digit * digit);
-            n /= 10;
-        }
-        System.out.println((sum == temp) ? (temp + " is an Amstrong Number") : (temp + " is not an Amstrong Number"));
+    //     int n = 1634;
+    //     int temp = n, sum = 0;
+    //     while (n > 0) {
+    //         int digit = n % 10;
+    //         sum = sum + (digit * digit * digit * digit);
+    //         n /= 10;
+    //     }
+    //     System.out.println((sum == temp) ? (temp + " is an Amstrong Number") : (temp + " is not an Amstrong Number"));
+
+
+        
     }
 }
