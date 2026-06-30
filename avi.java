5206e21dafe5a64d4480a657d8df5df654d2a6c9
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 11:31:11 +0000

    Jun 30, 2026, 5:01 PM

--- avi.java
+++ avi.java
@@ -6,7 +6,14 @@ public class Avi {
         // String rev = new StringBuilder(s).reverse().toString();
         // System.out.println((s.equals(rev)) ? "palindrome" : "not palindrome");
 
-        
+        int n = 121;
+        int temp = n, rev = 0;
+
+        while (temp != 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+        System.out.println((temp == rev) ? "Palindrome number" : "Not a Plindrome Number");
 
     }
 }
