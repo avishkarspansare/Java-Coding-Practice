9594076bbd11af0aa9e618006234af2d4ab84ea4
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 09:50:52 +0000

    Jun 30, 2026, 3:20 PM

--- avi.java
+++ avi.java
@@ -2,13 +2,13 @@
 public class Avi {
 
     public static void main(String[] args) {
-        int n = 101;
+        int n = 1901;
         int temp = n, rev = 0;
 
         while (n != 0) {
             rev = rev * 10 + n % 10;
             n /= 10;
         }
-        System.out.println((temp == rev) ? "palindrome" : "not palindrome");
+        System.out.println((temp == rev) ? temp : rev);
     }
 }
