9f8f37dfb98bb9d8cdd1d806c2c05e85984c96a0
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 11:45:33 +0000

    Jun 30, 2026, 5:15 PM

--- avi.java
+++ avi.java
@@ -2,7 +2,16 @@
 public class Avi {
 
     public static void main(String[] args) {
-        
+        int n = 5;
+        boolean isPrime = true;
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                isPrime = false;
+            }
+        }
+        System.out.println((isPrime == true) ? "Prime Number" : "Not a Prime Number");
 
     }
 }
