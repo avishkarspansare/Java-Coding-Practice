2d77e449d94a511c335e53e4c61ab9850ead9210
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 12:40:06 +0000

    Jun 30, 2026, 6:10 PM

--- reversestring.java
+++ reversestring.java
@@ -9,15 +9,15 @@ public class reversestring {
         String s = sc.next();
         String rev = "";
         //method 1 - O(n^2)
-        for (int i = s.length()-1; i>=0;i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             rev += s.charAt(i);
+            System.out.println(rev);
         }
         System.out.println(rev);
 
         // //method2 - O(n)
         // String rev2 = new StringBuilder(s).reverse().toString();
         // System.out.println(rev2);
-
     }
 
 }
