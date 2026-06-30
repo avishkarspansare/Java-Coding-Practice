a725de819fd96a98f4dfe64de460c01474c2541a
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 13:18:05 +0000

    Jun 30, 2026, 6:48 PM

--- avi.java
+++ avi.java
@@ -2,11 +2,6 @@
 public class Avi {
 
     public static void main(String[] args) {
-        String s = "Avi";
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev += s.charAt(i);
-        }
-        System.out.println(rev);
+        
     }
 }
