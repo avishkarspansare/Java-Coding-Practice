1e31af6ba242dc021a3f57988f3b722dcdf67e8c
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 13:35:44 +0000

    Jun 30, 2026, 7:05 PM

--- avi.java
+++ avi.java
@@ -1,22 +1,7 @@
-import java.util.Scanner;
 
 public class Avi {
-    private String name;
-
-    public void setname(String name) {
-        this.name = name;
-    }
-
-    public String getname() {
-        return this.name;
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        Avi a = new Avi();
-        a.setname(s);
-        System.out.println(a.getname());
-        sc.close();
+        
 
     }
 }
