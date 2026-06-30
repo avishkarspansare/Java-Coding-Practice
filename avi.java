948be8e4079b1dcc48910c1255774936b788f3c3
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 13:21:27 +0000

    Jun 30, 2026, 6:51 PM

--- avi.java
+++ avi.java
@@ -1,7 +1,18 @@
 
 public class Avi {
 
+    private int n;
+
+    public void setn(int n) {
+        this.n = n;
+    }
+
+    public int getn() {
+        return this.n;
+    }
+
     public static void main(String[] args) {
+
         
     }
 }
