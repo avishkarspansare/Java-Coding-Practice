7000f5e8b6aade112cabe723052c9a6a74cf4f36
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 08:08:47 +0000

    Jun 30, 2026, 1:38 PM

--- Inheritance.java
+++ Inheritance.java
@@ -1,35 +1,41 @@
 
 class Animal {
-    public void eat(){
+
+    public void eat() {
         System.out.println("Eat");
     }
 }
 
 //single
 class Dog extends Animal {
+
     public void makeSound() {
-    System.out.println("Bhaw Bhaw");
+        System.out.println("Bhaw Bhaw");
     }
 }
 //hierarchical + multilevel
-class Puppy extends Dog{
+
+class Puppy extends Dog {
+
     @Override
     public void makeSound() {
-    System.out.println("vuff vuff");
+        System.out.println("vuff vuff");
     }
 }
 
 //hierarchical
-class Cat extends Dog{  
+class Cat extends Dog {
+
     @Override
-    public void makeSound(){
+    public void makeSound() {
         System.out.println("meaw meaw");
     }
 }
 
 public class Inheritance {
+
     public static void main(String[] args) {
-        Puppy myPuppy = new  Puppy();
+        Puppy myPuppy = new Puppy();
         Dog myDog = new Dog();
         Cat myCat = new Cat();
         myPuppy.eat();
