50d71d73147db6ef71bfea870663bb935bbb85dd
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 17:20:39 +0000

    Jun 30, 2026, 10:50 PM

--- avi.java
+++ avi.java
@@ -1,4 +1,8 @@
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Avi {
 
     public static void main(String[] args) {
@@ -56,5 +60,14 @@ public class Avi {
 }
 
 class Solution {
-    
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length;i++){
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment)){
+                return new int[]{map.get(compliment), 1};
+            }
+        }
+        return new int[] {};
+    }
 }
