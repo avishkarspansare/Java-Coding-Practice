4d15c9792b0d781c20768504724894e855013e92
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-30 08:17:42 +0000

    Jun 30, 2026, 1:47 PM

--- avi.java
+++ avi.java
@@ -1,13 +1,9 @@
 
-public class avi {
-public volatile int a = 0;
-        
+public class Avi {
+
+    public volatile int a = 0;
+
     public static void main(String[] args) {
-        Thread t = new Thread();
-        
-        avi obj = new avi();
-        int s = obj.a++;
-        System.out.println(s);
 
     }
 }
