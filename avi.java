fedbe46377b9589f3b5a8a828708ca87ef4f6bfa
Avishkar Pansare <avishkarspansare@gmail.com>
2026-07-01 09:29:49 +0000

    Jul 1, 2026, 2:59 PM

--- avi.java
+++ avi.java
@@ -68,6 +68,18 @@ public class Avi {
         //     a = b;
         //     b = c;
         // }
+        //
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        sysout
     }
 }
 
