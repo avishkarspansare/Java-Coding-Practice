b9acb3c33f7b4a249c89241907be71474f25b8a3
Avishkar Pansare <avishkarspansare@gmail.com>
2026-07-01 01:21:16 +0000

    Jul 1, 2026, 6:51 AM

--- avi.java
+++ avi.java
@@ -1,7 +1,4 @@
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Avi {
 
@@ -56,23 +53,23 @@ public class Avi {
         // }
         // System.out.println((sum == temp) ? "Amstrong" : "Not Amstrong");
         //
-        Solution s = new Solution();
-        int[] arr = s.twoSum(new int[]{1,2,3,4,5}, 8);
-        System.out.println(Arrays.toString(arr));
+        // Solution s = new Solution();
+        // int[] arr = s.twoSum(new int[]{1,2,3,4,5}, 8);
+        // System.out.println(Arrays.toString(arr));
     }
 }
 
-class Solution {
+// class Solution {
 
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int compliment = target - nums[i];
-            if(map.containsKey(compliment)) {
-                return new int[]{map.get(compliment), i};
-            }
-            map.put(nums[i], i);
-        }
-        return new int[]{};
-    }
-}
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             int compliment = target - nums[i];
+//             if(map.containsKey(compliment)) {
+//                 return new int[]{map.get(compliment), i};
+//             }
+//             map.put(nums[i], i);
+//         }
+//         return new int[]{};
+//     }
+// }
