94aaac0875cc3bfb513bbcc24b90c2f410709d1c
Avishkar Pansare <avishkarspansare@gmail.com>
2026-07-01 08:19:04 +0000

    Jul 1, 2026, 1:49 PM

--- avi.java
+++ avi.java
@@ -62,7 +62,7 @@ public class Avi {
         //
         int n = 8;
         int a = 0, b = 1;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.println(a + " ");
             int c = a + b;
             a = b;
